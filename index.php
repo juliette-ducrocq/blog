28329f30b713dd7e19fe5c8bdba8d8176d77c00a
Juliette Ducrocq <julietteducrocq@MacBook-Pro-de-Juliette.local>
2019-05-03 15:03:58 +0200

    flex

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
   <div class="content">
     <section class="articles">
     <?php for($i=0; $i<=5; $i++) : ?>
-      <article class="article a">
+      <article class="article">
         <h2>Article 1</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
